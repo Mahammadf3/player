b701b98bbffaf3334575fc1f551658d5ae6529cc
Mahammadf3 <shaikmahammad622@gmail.com>
2023-10-09 11:19:46 +0530

    first commit

---
+++ valley.sql
@@ -0,0 +1,10 @@
+---create table valleyball(id int, name varchar(200),age int,matches_play int, match_win int);
+
+---insert into valleyball(id,name,age,matches_play,match_win)
+--values
+---(1,'mahammad',22,60,51),
+---(2,'Raj',24,67,39),
+---(3,'mahesh',28,29,6),
+---(4,'huzaifa',27,55,31);
+
+select * from valleyball;
